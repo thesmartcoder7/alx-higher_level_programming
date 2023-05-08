6734c516b081b314b4d85eca627f7d4155f7e6bb
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-08 20:00:34 +0300

    chore: create the python bytecode script

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-# Here's what that code does, step by step:
-# - Take the number 98 and store it in the computer's memory.
-# - Take two more numbers, called "a" and "b", and store them in the computer's memory too.
-# - Raise the number "a" to the power of the number "b".
-# - Add the number 98 to the result of step 3.
-# - Give the final answer back to whoever asked for it.
-
 def magic_calculation(a, b):
     return 98 + (a ** b)
