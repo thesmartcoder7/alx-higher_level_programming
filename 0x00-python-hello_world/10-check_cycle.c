0016ca87708cb76cd4952c569c096c48edc21b87
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-08 19:09:46 +0300

    chore: update check function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,26 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * check_cycle - this checks if singly linked list is a cycle
- * Return: 0 if no cycle, 1 is yes
+ * check_cycle - checks if a singly linked list has a cycle in it
  * @list: list head
-*/
+ * Return: 0 if no, 1 if yes
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *fast, *slow = list;
 
-	if (!list)
+	if (list == NULL)
 		return (0);
 
-	while (1)
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		if (fast->next != NULL && fast->next->next != NULL)
-		{
-			fast = fast->next->next;
-			slow = slow->next;
-
-			if (fast == slow)
-				return (1);
-		}
-		else
-			return (0);
+		if (slow == fast)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
 	}
+	return (0);
 }
