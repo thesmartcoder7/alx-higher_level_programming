23b2d92e851887aff95a436457e9321b4f38700a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-09 16:05:36 +0300

    chore: patch checker corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(99):
-    print("0{d}, ".format(i) if len(str(i)) == 1 else f"{i}, ", end='')
+    print("0{s}, ".format(i) if len(str(i)) == 1 else f"{i}, ", end='')
 
 print("{s}".format(str(99)))
