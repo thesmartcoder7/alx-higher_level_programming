6f695c66a24f70f85678d4643e427020e708b9f0
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-09 08:28:40 +0300

    chore: print a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    temp = []
+    for char in str:
+        try:
+            if isinstance(int(char), int):
+                temp.append(char)
+        except:
+            if char == char.lower() and char != " ":
+                temp.append(chr(ord(char) - 32))
+            else:
+                temp.append(char)
+    
+    print (''.join(temp))
