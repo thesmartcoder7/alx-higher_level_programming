a55550d853434a415d16a960887bbeb35b7c61ce
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-09 18:41:12 +0300

    chore: update last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ else:
     d = n % 10
 
 if d > 5:
-    print("Last d of {} is {} and is greater than 5".format(n, d))
+    print("Last digit of {} is {} and is greater than 5".format(n, d))
 elif d == 0:
-    print("Last d of {} is {} and is 0".format(n, d))
+    print("Last digit of {} is {} and is 0".format(n, d))
 else:
-    print("Last d of {} is {} and is less than 6 and not 0".format(n, d))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(n, d))
