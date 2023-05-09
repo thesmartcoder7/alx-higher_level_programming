a697cbffa85637e25e7f5a76a92675f69b4fe086
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-09 14:46:01 +0300

    chore: patch negative input

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)
+i = random.randint(-10000, 10000)
 
-last_digit = int(str(number)[-1])
+ld = int(str(i)[-1])
 
-if last_digit > 5 and number > 0:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+if ld > 5 and i > 0:
+    print(f"Last digit of {i} is {ld} and is greater than 5")
+elif ld == 0:
+    print(f"Last digit of {i} is {ld} and is 0")
+elif i < 0:
+    print(f"Last digit of {i} is -{ld} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {last_digit}\
-           and is less than 6 and not 0")
+    print(f"Last digit of {i} is {ld} and is less than 6 and not 0")
