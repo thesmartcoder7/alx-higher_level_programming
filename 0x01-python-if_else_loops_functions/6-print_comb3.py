4805ea2bd6bf6464437350438b0051ea966b124a
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-09 16:07:26 +0300

    chore: patch checker corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for one in range(10):
     for two in range(10):
         if int(f"{one}{two}") < int(f"{two}{one}"):
             if f"{one}{two}" != str(89):
-                print("{d}{d}, ".format(one, two), end='')
+                print("{}{}, ".format(one, two), end='')
             else:
-                print("{d}{d}, ".format(one, two))
+                print("{}{}, ".format(one, two))
