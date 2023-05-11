057a2dc1d0cce4c94fb53529616b30d866316db2
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-11 10:00:18 +0300

    chore: create a function the imports functions from a different file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+cal = __import__("calculator_1")
+
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
