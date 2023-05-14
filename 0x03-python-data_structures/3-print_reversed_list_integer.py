49f9e4ecab2edca6fc8f93927352c1fe67906466
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-14 03:29:01 +0300

    chore: print items of a reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    new_list = list(reversed(my_list))
-    for item in new_list:
+    for item in reversed(my_list):
         print("{:d}".format(item))
