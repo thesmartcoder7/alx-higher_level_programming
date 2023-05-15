4d17ae57ea3215dbb00a021d4104a5d6a61dd3ae
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-16 01:34:32 +0300

    chore: delete an item at a specific index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,10 +5,5 @@ def delete_at(arr, idx):
     if idx >= len(arr) or idx < 0:
         return arr
     else:
-        new = []
-        for i in range(len(arr)):
-            if i == idx:
-                continue
-            else:
-                new.append(arr[i])
-        return new
+        del (arr[idx])
+        return arr
