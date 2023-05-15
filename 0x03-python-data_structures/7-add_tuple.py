df845a7ef96a609e8ce33b4333d57c872f3caed8
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-16 00:54:27 +0300

    chore: add two tuples and return a tuple with two integers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,21 +4,22 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = []
 
+    while len(tuple_a) < 2:
+            tuple_a += (0,)
+
     if len(tuple_a) == len(tuple_b):
         for i in range(len(tuple_a)):
             new_tuple.append(tuple_a[i] + tuple_b[i])
-            # print(f"debug {new_tuple} from first loop")
 
     elif len(tuple_b) > len(tuple_a):
         temp = [tuple_b[0], tuple_b[1]]
         for i in range(len(tuple_a)):
             new_tuple.append(tuple_a[i] + temp[i])
-            # print(f"debug {new_tuple} from second loop")
+
     else:
         while len(tuple_b) < len(tuple_a):
             tuple_b += (0,)
         for i in range(len(tuple_a)):
             new_tuple.append(tuple_a[i] + tuple_b[i])
-            # print(f" debug {new_tuple} from third loop")
 
     return tuple(new_tuple)
