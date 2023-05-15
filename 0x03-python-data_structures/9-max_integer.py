0602997fcd4bcdf05d5f363568453f66598686ab
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-16 01:13:44 +0300

    chore: find a maximum number from list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,11 +6,11 @@ def max_integer(my_list=[]):
         This function finds the biggers integer
         from a given list
     """
-    if len(my_list):
+    if len(my_list) == 0:
+        return None
+    else:
         max = 0
         for item in my_list:
             if item > max:
                 max = item
         return max
-    else:
-        return None
