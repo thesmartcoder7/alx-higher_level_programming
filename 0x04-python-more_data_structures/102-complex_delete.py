80bcfb2e1cda5250e2a339c62e4c89d1205e9f98
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-17 23:40:29 +0300

    chore: delete keys with a spacific value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,8 +5,8 @@ def complex_delete(a_dict, val):
     if not val or val is None:
         return a_dict
 
-    for key in a_dict:
-        if a_dict[key] == val:
+    for key, value in dict(a_dict).items():
+        if value == val:
             del a_dict[key]
             break
 
