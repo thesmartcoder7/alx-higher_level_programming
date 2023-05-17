00b2f75cb352d033c20b8647355cfb5cc43359bf
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-18 00:17:14 +0300

    chore: correct restrictions on functions allowed

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,70 +4,65 @@
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
-/**
- * print_python_list - Prints basic info about Python lists.
- * @p: A PyObject list object.
- */
-void print_python_list(PyObject *p)
-{
-    Py_ssize_t size = PyList_Size(p);
+void print_python_list(PyObject *p) {
+    PyObject *list_size = PyObject_Length(p);
+    PyObject *allocated = PyObject_GetAttrString(p, "allocated");
+    Py_ssize_t size = PyLong_AsSsize_t(list_size);
     Py_ssize_t i;
 
     printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", PyLong_AsSsize_t(allocated));
 
-    for (i = 0; i < size; i++)
-    {
+    for (i = 0; i < size; i++) {
         PyObject *item = PyList_GetItem(p, i);
-        const char *type = Py_TYPE(item)->tp_name;
+        PyObject *item_type = PyObject_GetAttrString(item, "__class__");
+        PyObject *type_name = PyObject_GetAttrString(item_type, "__name__");
 
-        printf("Element %ld: %s\n", i, type);
+        printf("Element %zd: %s\n", i, PyUnicode_AsUTF8(type_name));
+
+        if (strcmp(PyUnicode_AsUTF8(type_name), "bytes") == 0)
+            print_python_bytes(item);
+
+        Py_DECREF(item_type);
+        Py_DECREF(type_name);
     }
+
+    Py_DECREF(list_size);
+    Py_DECREF(allocated);
 }
 
-/**
- * print_python_bytes - Prints basic info about Python byte objects.
- * @p: A PyObject byte object.
- */
-void print_python_bytes(PyObject *p)
-{
-    Py_ssize_t size;
+void print_python_bytes(PyObject *p) {
+    PyObject *size_obj = PyObject_Length(p);
+    Py_ssize_t size = PyLong_AsSsize_t(size_obj);
     Py_ssize_t i;
-    char *bytes;
-    char *byte_repr;
+    char *bytes_repr;
 
-    printf("[.] Bytes object info\n");
+    printf("[.] bytes object info\n");
 
-    if (!PyBytes_Check(p))
-    {
+    if (!PyBytes_Check(p)) {
         printf("  [ERROR] Invalid Bytes Object\n");
+        Py_DECREF(size_obj);
         return;
     }
 
-    size = PyBytes_Size(p);
-    bytes = PyBytes_AsString(p);
-
-    printf("  Size: %ld\n", size);
-    printf("  Trying string: %s\n", bytes);
+    printf("  size: %zd\n", size);
 
     if (size > 10)
         size = 10;
 
-    byte_repr = malloc((size * 4) + 1);
-    if (byte_repr == NULL)
-    {
-        printf("  [ERROR] Failed to allocate memory\n");
-        return;
+    bytes_repr = PyBytes_AsString(p);
+
+    printf("  trying string: %s\n", bytes_repr);
+
+    printf("  first %zd bytes: ", size);
+    for (i = 0; i < size; i++) {
+        printf("%02hhx", bytes_repr[i]);
+        if (i == size - 1)
+            printf("\n");
+        else
+            printf(" ");
     }
 
-    for (i = 0; i < size; i++)
-    {
-        sprintf(byte_repr + (i * 4), "%.2hhx ", bytes[i]);
-    }
-    byte_repr[size * 4] = '\0';
-
-    printf("  First %ld bytes: %s\n", size, byte_repr);
-
-    free(byte_repr);
+    Py_DECREF(size_obj);
 }
