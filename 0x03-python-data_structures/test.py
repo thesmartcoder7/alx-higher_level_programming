c188560f5aa4e106fac97a19a938068585e83728
samuel martins <samuel.martins4.sm@gmail.com>
2023-05-21 22:41:33 +0300

    test: add the replace item at list testcase

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -24,6 +24,21 @@ class TestElementAt(unittest.TestCase):
         element = element_at(my_list, idx)
 
         self.assertEqual(element, 4)
+
+
+
+# replace item in list
+class TestReplaceInList(unittest.TestCase):
+    def test_replace_at(self):
+        replace_in_list = __import__('2-replace_in_list').replace_in_list
+
+        my_list = [1, 2, 3, 4, 5]
+        idx = 3
+        new_element = 9
+        new_list = replace_in_list(my_list, idx, new_element)
+        expected_list = [1, 2, 3, 9, 5]
+
+        self.assertEqual(new_list, expected_list)
         
 
 
