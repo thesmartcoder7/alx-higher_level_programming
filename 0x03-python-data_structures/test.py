b49e0efe0f473f2e7fcf31d710022ecc39b9daa7
samuel martins <samuel.martins4.sm@gmail.com>
2023-05-21 23:30:56 +0300

    test: create the max integer test

--- 0x03-python-data_structures/test.py
+++ 0x03-python-data_structures/test.py
@@ -102,5 +102,19 @@ class TestMultipleReturn(unittest.TestCase):
 
 
 
+# return max integer from list
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        max_integer = __import__('9-max_integer').max_integer
+
+        my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+        max_value = max_integer(my_list)
+
+        self.assertIsInstance(max_value, int)
+        self.assertEqual(max_value, 90)
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
