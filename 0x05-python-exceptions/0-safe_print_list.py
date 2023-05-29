1b7cb1be260b2b0d37eece11d10d1180690999e0
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-29 21:34:42 +0300

    chore: correct implementation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,20 +1,38 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
+
+def get_length(arr):
+    """
+        Get the length of any list
+    """
+    length = 0
+    try:
+        for item in arr:
+            length += 1
+        return length
+    except TypeError as error:
+        print(error)
+        raise
 
 
 def safe_print_list(arr, items):
     """
         Print certain elements of a list
     """
-    j = 0
-
+    length = get_length(arr)
     try:
-        for i in arr:
-            if j < items:
-                print('{}'.format(arr[j]), end='')
-                j += 1
-
-        print()
-    except TypeError:
-        pass
-    finally:
-        return j
+        printed = 0
+        if items > length:
+            for i in range(length):
+                print('{}'.format(arr[i]), end='')
+                printed += 1
+            print()
+        else:
+            for i in range(items):
+                print('{}'.format(arr[i]), end='')
+                printed += 1
+            print()
+        return printed
+    except IndexError as error:
+        print(error)
+        raise
