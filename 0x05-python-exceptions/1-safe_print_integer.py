97797acb7d99476a91ff07732cec032966c88ef5
samuel martins <samuel.martins4.sm@gmail.com>
2023-05-29 22:30:19 +0300

    chore: print integers safely

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,13 +2,8 @@
 
 
 def safe_print_integer(value):
-    """
-        Print integers
-    """
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
-    except TypeError as error:
-        print(error)
+        print('{:d}'.format(value))
+        return True
+    except (TypeError, ValueError):
         return False
