fe215ad99b1acb19faffc7a242df9313193a9059
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-31 02:59:47 +0300

    chore: add public instance method to print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""This class defines a square"""
+
+
+class Square:
+    """This class defines an empty square"""
+
+    def __init__(self, size=0):
+        """class initializer method with data"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of the size property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the size area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """Prints to stdout the square with the character '#'."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
