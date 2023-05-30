d9e4a12d28412777e5bbff014de8539254d9082b
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-31 02:50:55 +0300

    chore: create an instance method to calculate the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This class defines a square"""
+
+
+class Square:
+    """This class defines an empty square"""
+
+    def __init__(self, size=0):
+        """class initializer method with data"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the size area of the square"""
+        return self.__size**2
