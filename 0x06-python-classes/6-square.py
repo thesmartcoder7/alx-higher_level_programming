946800862980fe1c0da96cd9379a11a335e428f0
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-31 03:15:22 +0300

    chore: update the position property of the class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
-"""This class defines a square"""
+"""
+    This is a square blue print.
+
+    Private instance attributes:
+        - size
+        - property
+
+    public instance methods:
+        - property def size(self)
+        - property setter def size(self, value)
+        - property def position(self)
+        - property setter def position(self, value)
+        - def area(self)
+        - def my_print(self)
+"""
 
 
 class Square:
@@ -8,10 +22,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """class initializer method with data"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
