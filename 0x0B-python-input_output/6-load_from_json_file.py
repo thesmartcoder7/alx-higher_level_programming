2ecf3bed4d9a7c8229c9bd1fd2c776614ff3acd9
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-14 02:44:07 +0300

    chore: create an object from a json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defining load_from_json_file function"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an object from json file"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        return json.load(file)
