0524d40ea7a52a286dac8cf3acfc386019869739
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-06-14 02:50:43 +0300

    chore: define a student based on 9-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Contains the clas "Student"""
+
+
+class Student:
+    """The Class Representation of a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            returns a dictionary representation of the instance
+            with specified attributes
+        """
+        if not attrs:
+            return self.__dict__
+        new_dict = {}
+        for att in attrs:
+            try:
+                new_dict[att] = self.__dict__[att]
+            except KeyError:
+                pass
+        return new_dict
