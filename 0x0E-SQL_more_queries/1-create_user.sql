a8935a533a262a76d08e6628fe65602ddb8edc4f
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-07-20 04:17:43 +0300

    fix: update corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,3 @@
 -- creates the MySQL server user user_0d_1
-
--- Check if the user already exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1');
-
--- If the user doesn't exist, create the user and grant privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
