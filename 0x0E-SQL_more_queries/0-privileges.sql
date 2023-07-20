9f819b687b39add2ea0a7fc8f9eb6f86ff5bda54
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-07-20 04:25:04 +0300

    fix: update corrections

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- lists all privileges MySQL Users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- 0-privileges.sql
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
