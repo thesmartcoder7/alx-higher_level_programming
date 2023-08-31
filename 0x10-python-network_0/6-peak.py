8d5c66bf8d388937225440c7dfcbadf9a9cd8041
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-09-01 02:11:04 +0300

    chore: find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""task 6 model"""
+"""
+A function that finds a peak in a list of unsorted integers.
+"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
-        return None
-
+    """
+    Find the highest point within an array of integers that are not sorted.
+    """
     size = len(list_of_integers)
-    if size == 1:
+
+    if size == 0:
+        return None
+    elif size == 1:
         return list_of_integers[0]
     elif size == 2:
         return max(list_of_integers)
 
-    middle = int(size / 2)
+    middle = size // 2
     peak = list_of_integers[middle]
+
     if peak > list_of_integers[middle - 1] and peak > list_of_integers[middle + 1]:
         return peak
     elif peak < list_of_integers[middle - 1]:
