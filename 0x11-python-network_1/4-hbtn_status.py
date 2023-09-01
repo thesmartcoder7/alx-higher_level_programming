6827a89f4531b29b1d3ede348ca79a9a32127fe5
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-09-02 00:54:53 +0300

    chore: update the status code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,8 @@
 """fetches https://intranet.hbtn.io/status."""
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
