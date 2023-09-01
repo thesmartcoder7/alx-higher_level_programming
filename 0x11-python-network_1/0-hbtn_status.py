de62418779c200675f8e5dd84f84698c0c459e18
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-09-02 00:43:58 +0300

    chore: implement the network project

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A script that fetches https://intranet.hbtn.io/status.
+using urlib package
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+lists the 10 most recent commits on a given GitHub repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
