9b6879e99e5c035e9640a3bd25eac2e20789d1a3
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-09-27 00:28:22 +0300

    chore: update the characters script

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const req = require("request");
+const req = require('request');
 const id = process.argv[2];
-const url = "https://swapi-api.hbtn.io/api/films/";
+const url = 'https://swapi-api.hbtn.io/api/films/';
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
